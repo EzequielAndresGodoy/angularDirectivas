c196fcd867b6050f6463224012acc7785abdb4a3
Ezequiel Godoy <godoy.andres.ezequiel@gmail.com>
2022-11-09 17:23:55 -0300

    ngstyle y ngclass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+type inputs = {
+  titulo:string
+  }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angularDirectivas';
+
+
+
+  title = 'Registro de usuario';
+  mensaje = '';
+  registrado = false;
+  nombre = "";
+  apellido = "";
+  entradas: Array<inputs>;
+
+  constructor() {
+    this.entradas = [
+      {titulo: "Python"},
+      {titulo: "Java"},
+      {titulo: "Angular"},
+      {titulo: "JavaScript"},
+    ]
+  }
+
+  registrarUsuario() {
+    this.mensaje = "Ahora esta registrado!"
+    this.registrado = true
+  }
 }
